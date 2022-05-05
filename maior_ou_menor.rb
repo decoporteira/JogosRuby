0acfd74b3e1b911ce657bb48c4ed216727b8c942
Andre Pereira <andreporteira@gmail.com>
2022-05-05 14:40:46 -0300

    Refatoracao

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -8,35 +8,48 @@ end
 
 def sorteia_numero_secreto
     puts "Escolha um número secreto entre 0 e 200"
-    numero_secreto = 175
+    sorteado = 175
     puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
-    return numero_secreto
+    sorteado
 end
+
+def pede_um_numero(tentativa, limite_de_tentativas)
+    puts "\n\n\n\n\n\n"
+    puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+    puts "Entre com o número"
+    chute = gets
+    puts "Será que acertou? Você chutou " + chute
+    chute.to_i
+end
+
+def verifica_se_acertou(numero_secreto,chute)
+    acertou = numero_secreto == chute
+    
+    if acertou   
+        puts "Você acertou, o número secreto era " + numero_secreto.to_s  
+        return true
+    end
+    
+        maior = numero_secreto > chute
+        if maior
+            puts "O número secreto é maior que " + chute.to_s
+        else
+            puts "O número secreto é menor que " + chute.to_s
+        end
+        false
+     
+end
+
 
 da_boas_vindas
 numero_secreto = sorteia_numero_secreto
 
 limite_de_tentativas = 3
-
-for tentativa in 1..limite_de_tentativas   
-    puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
-    puts "Entre com o número"
-    chute = gets
-    puts "Será que acertou? Você chutou " + chute
-    
-    acertou = numero_secreto == chute.to_i
-    
-    if acertou   
-        puts "Você acertou, o número secreto era " + numero_secreto.to_s  
+for tentativa in 1..limite_de_tentativas     
+    chute = pede_um_numero(tentativa, limite_de_tentativas)
+    if verifica_se_acertou(numero_secreto, chute) 
         break
-    else
-        maior = numero_secreto > chute.to_i
-        if maior
-            puts "O número secreto é maior que " + chute
-        else
-            puts "O número secreto é menor que " + chute
-        end
-    end 
+    end
 end
 
 
