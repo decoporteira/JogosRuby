c6db482efc6f23ca052190e5edd4a6f88aa31564
Andre Pereira <andreporteira@gmail.com>
2022-05-06 17:10:09 -0300

    array

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -13,9 +13,10 @@ def sorteia_numero_secreto
     sorteado
 end
 
-def pede_um_numero(tentativa, limite_de_tentativas)
-    puts "\n\n\n\n\n\n"
+def pede_um_numero(chutes, tentativa, limite_de_tentativas)
+    puts "....................."
     puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+    puts "Chutes até agora: " + chutes.to_s
     puts "Entre com o número"
     chute = gets
     puts "Será que acertou? Você chutou " + chute
@@ -45,12 +46,22 @@ da_boas_vindas
 numero_secreto = sorteia_numero_secreto
 
 limite_de_tentativas = 3
+chutes = []
+
+total_de_chutes = 0
 for tentativa in 1..limite_de_tentativas     
-    chute = pede_um_numero(tentativa, limite_de_tentativas)
+    chute = pede_um_numero(chutes, tentativa, limite_de_tentativas)
+    
+    
+    chutes[total_de_chutes] = chute
+    total_de_chutes += 1
+
     if verifica_se_acertou(numero_secreto, chute) 
         break
     end
 end
+puts "Suas tentativas foram: "
+puts chutes
 
 
 
