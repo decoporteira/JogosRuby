52d8e9e88d07229132d46024d35dc15b84a6a190
Andre Pereira <andreporteira@gmail.com>
2022-05-19 19:17:35 -0300

    Jogo forca funcionando e sem repetição de letras

--- forca.rb
+++ forca.rb
@@ -1,4 +1,4 @@
-def de_boas_vindas
+def da_boas_vindas
   puts "Bem vido ao jogo de forca"
   puts "Qual seu nome?"
   nome = gets.strip 
@@ -19,20 +19,6 @@ def nao_quer_jogar?
   nao_quero_jogar = quero_jogar.upcase == "N"
 end
 
-def joga(nome)
-  palavra_secreta = sorteia_palavra_secreta
-  erros = 0
-  chutes =[]
-  pontos_ate_agora = 0
-
-  while erro < 5
-    chute = pede_um_chute(chutes, erros) 
-    chutes << chute end
-    #entrar com regras de acerto aqui
-  end
-  puts "Você ganhou #{pontos_ate_agora}"
-end
-
 def pede_um_chute (chutes, erros)
   puts "\n\n\n\n"
   puts "Erros até agora: #{erros}"
@@ -42,6 +28,49 @@ def pede_um_chute (chutes, erros)
   puts "Será que acertou? Você chutou #{chute}"
   chute
 end
+
+def joga(nome)
+  palavra_secreta = sorteia_palavra_secreta
+  erros = 0
+  chutes =[]
+  pontos_ate_agora = 0
+ 
+
+  while erros < 5
+    chute = pede_um_chute(chutes, erros) 
+    if chutes.include? chute
+      puts "Você já chutou #{chute}"
+      next #pula para a próxima rodada do while
+    end
+      chutes << chute 
+    chutou_uma_unica_letra = chute.size == 1
+    if chutou_uma_unica_letra
+      total_encontrado = palavra_secreta.count(chute)
+      puts chute
+      if total_encontrado == 0
+        puts "Letra não encontrada"
+        erros += 1
+      else
+        puts "Letra encontrada #{total_encontrado} vezes!"
+      end
+       
+    else
+        acertou = chute == palavra_secreta
+        if acertou
+          puts "Parabéns! Acertou a palavra!"
+          pontos_ate_agora += 100
+          break
+        else
+            puts "Que pena... errou!"
+            pontos_ate_agora -=30
+            erros += 1
+          end 
+      end
+    end
+  puts "Você ganhou #{pontos_ate_agora}"
+end
+
+
 
 nome = da_boas_vindas
 
